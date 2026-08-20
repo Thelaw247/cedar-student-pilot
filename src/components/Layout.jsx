548f6e60c2a7cbba482fb32e901098f4e3308ec1
De wet Luus <dewetluus@gmail.com>
2026-08-20 02:35:23 +0000

    External agent changes

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import React, { useEffect, useState, useCallback } from 'react';
 import { Outlet } from 'react-router-dom';
 import Sidebar from './Sidebar';
 import BottomNav from './BottomNav';
+import ClassStatusBar from './ClassStatusBar';
 import StudySessionNotifier from './StudySessionNotifier';
 import OfflineIndicator from './OfflineIndicator';
 import CommandPalette from './CommandPalette';
@@ -37,6 +38,7 @@ export default function Layout() {
       <Sidebar />
       <div className="flex-1 flex flex-col min-w-0">
         <main className="flex-1 pb-20 lg:pb-0">
+          <ClassStatusBar variant="mobile" />
           <Outlet />
         </main>
         <BottomNav />
